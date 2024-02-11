1c4ee1942b546d8615077100bf8c904d21488e0e
fnguimdo <87722677+fnguimdo@users.noreply.github.com>
2024-02-11 12:59:51 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           alt="logo"
         />
         <div>Congratulations!!!</div>
-        <div>Thank you for attending prod guide deployment playback session!</div>
+        <div>Thank you for attending this session!</div>
       </header>
     </div>
   );
